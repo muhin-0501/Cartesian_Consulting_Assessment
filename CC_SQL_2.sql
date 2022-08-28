0efbe07c46eaf09b74a1928aa346e0d17117ec47
MUHIN V <103983317+muhin-0501@users.noreply.github.com>
2022-08-28 17:14:02 +0530

    Add files via upload

---
+++ CC_SQL_2.sql
@@ -0,0 +1,20 @@
+#ASSIGNMENT-02SQL
+#Creating the customer table
+CREATE TABLE Customer (Customer_Id varchar(255));
+INSERT INTO customer VALUES ('Abhinash'),('Vipin'),('Mahesh'),('Bijoy'),('Bhabani'),('Ashutosh');
+
+#Creating the Voucher table
+CREATE TABLE Voucher (Voucher_Id varchar(255) UNIQUE);
+INSERT INTO Voucher VALUES('ABXFH'),('SDFGH'),('ERTYY'),('PPLKM');
+
+with cte as(select *,row_number() over(order by Customer_Id) rr from Customer),cte2 as(select *,row_number() over(order by Voucher_Id) rr from Voucher)
+    
+select Customer_Id Customer_Key,Voucher_Id Gift_Voucher_Key
+from cte c1
+left join cte2 c2 on c1.rr=c2.rr;
+
+
+
+
+
+
